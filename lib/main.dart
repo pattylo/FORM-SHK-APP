4df6b36c05026e6144ead04cd3fcc9cbafe385d1
Patrick Lo <19451945as@gmail.com>
2022-01-25 11:31:04 +0800

    1131

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,9 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.asset(
-          'utils/images/shk.png',
-          width: 120,
-          height: 120,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('sup'),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -39,3 +38,156 @@ class Home extends StatelessWidget {
 //           ),
 //         ),  
 //NetworkImage('https://pjreddie.com/media/image/sayit.jpg'),
+
+// child: Image.asset(
+//           'utils/images/shk.png',
+//           width: 120,
+//           height: 120,
+//         ),
+// child: Icon(
+//           Icons.airplane_ticket,
+//           color: Colors.blue,
+//           size: 120.0,
+//         ),
+
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(home: Home()));
+
+// class Home extends StatelessWidget {
+//   // const test({ Key? key }) : super(key: key);
+
+//   @override //we are overiding the original build function in original class
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('SHK 新鴻基'),
+//         centerTitle: true,
+//         backgroundColor: Colors.red[600],
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {},
+//           child: Text('sup'),
+          
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Text('click'),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(home: Home()));
+
+// class Home extends StatelessWidget {
+//   // const test({ Key? key }) : super(key: key);
+
+//   @override //we are overiding the original build function in original class
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('SHK 新鴻基'),
+//         centerTitle: true,
+//         backgroundColor: Colors.red[600],
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {},
+//           child: Text('sup'),
+          
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Text('click'),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(home: Home()));
+
+// class Home extends StatelessWidget {
+//   // const test({ Key? key }) : super(key: key);
+
+//   @override //we are overiding the original build function in original class
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('SHK 新鴻基'),
+//         centerTitle: true,
+//         backgroundColor: Colors.red[600],
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {},
+//           child: Text('sup'),
+          
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Text('click'),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(home: Home()));
+
+// class Home extends StatelessWidget {
+//   // const test({ Key? key }) : super(key: key);
+
+//   @override //we are overiding the original build function in original class
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('SHK 新鴻基'),
+//         centerTitle: true,
+//         backgroundColor: Colors.red[600],
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {},
+//           child: Text('sup'),
+          
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Text('click'),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }import 'package:flutter/material.dart';
+
+// void main() => runApp(MaterialApp(home: Home()));
+
+// class Home extends StatelessWidget {
+//   // const test({ Key? key }) : super(key: key);
+
+//   @override //we are overiding the original build function in original class
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('SHK 新鴻基'),
+//         centerTitle: true,
+//         backgroundColor: Colors.red[600],
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {},
+//           child: Text('sup'),
+          
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Text('click'),
+//         onPressed: () {},
+//       ),
+//     );
+//   }
+// }
