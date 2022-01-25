e37fff080275d34509d5d891c957c9cba8c0ded1
Patrick Lo <19451945as@gmail.com>
2022-01-25 10:29:23 +0800

    1029

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
 
-    appBar: AppBar(
-      title: Text('SHK 新鴻基'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-      
-      
-    ),
+void main() => runApp(MaterialApp(home: Home()));
 
-    body: Center(
-      child: Text(
-        '早晨!師傅師姐!',
-        style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-          letterSpacing:  2.0,
-          color: Colors.grey[600],
+class Home extends StatelessWidget {
+  // const test({ Key? key }) : super(key: key);
+
+  @override //we are overiding the original build function in original class
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SHK 新鴻基'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset(
+          'utils/images/shk.png',
+          width: 120,
+          height: 120,
         ),
       ),
-    ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
 
-    floatingActionButton: FloatingActionButton(
-      child: Text('click'),
-      onPressed: () {},
-    ),
 
-  ),
-));
+// Text(
+//           '早晨!師傅師姐!',
+//           style: TextStyle(
+//             fontSize: 25,
+//             fontWeight: FontWeight.bold,
+//             letterSpacing: 2.0,
+//             color: Colors.grey[600],
+//           ),
+//         ),  
+//NetworkImage('https://pjreddie.com/media/image/sayit.jpg'),
